d1d856c9c5da3bb58dce6b72ef46b8b5bfadf7c1
Justin Mei <imjustinmei@gmail.com>
2021-12-19 01:42:17 -0500

    port

--- server.js
+++ server.js
@@ -159,6 +159,6 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('listening on port 3000');
 });
