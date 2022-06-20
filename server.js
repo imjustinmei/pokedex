f169169169735ce884265919cb7de8903e53c0c6
Justin Mei <imjustinmei@gmail.com>
2022-06-20 18:36:03 -0400

    procfile and fix cache

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('listening on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
